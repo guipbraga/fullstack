0d7b73d29dbf605b669d464904798cbdbfbb3f9c
guipbraga <guipbraga@gmail.com>
2024-05-15 15:48:37 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,12 @@
+a_str = input('Digite sua altura em centímetros:')
+a_int = int(a_str)
+b_str = input('Digite seu peso em kilos:')
+b_int = int(b_str)
+soma = a_int+b_int
+print('Resultado da adição: '+str(soma))
+soma = a_int-b_int
+print('Resultado da subtração: '+str(soma))
+soma = a_int*b_int
+print('Resultado da multiplicação: '+str(soma))
+soma = a_int/b_int
+print('Resultado da divisão: '+str(soma))
